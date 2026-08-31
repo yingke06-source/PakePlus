2b3fd53dbac0a4c7a09607649387f7aa11c5eb8f
yingke06-source <yingke06@gmail.com>
2026-09-01 07:46:46 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,15 +1,21 @@
-console.log('custom.js ----')
-
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
-
-// 监听全屏变化
-document.addEventListener('fullscreenchange', () => {
-    console.log('fullscreenchange')
-})
-
-// Safari / WebKit
-document.addEventListener('webkitfullscreenchange', () => {
-    console.log('webkitfullscreenchange')
-})
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});const hookClick = (e) => {
+  const origin = e.target.closest('a')
+  const isBaseTargetBlank = document.querySelector('head base[target="_blank"]')
+  console.log('origin', origin, isBaseTargetBlank)
+  if (
+    (origin && origin.href && origin.href.length>0)
+  ) {
+    e.preventDefault()
+    let realUrl = origin.href
+    // 移除wloc8.com中转跳转
+    if(realUrl.includes('wloc8.com')){
+       const params = new URLSearchParams(realUrl.split('?')[1])
+       realUrl = params.get('url') || params.get('redirect') || realUrl
+    }
+    console.log('handle origin', realUrl)
+    location.href = realUrl
+  } else {
+    console.log('not handle origin',)
+  }
+}
+document.addEventListener('click',hookClick,true)
